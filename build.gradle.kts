3ad5357922afa6d239a38dde9800984a654de41c
saeed123124-dot <saeed123124@gmail.com>
2026-05-29 13:34:26 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
